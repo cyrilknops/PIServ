6f031ca1a07071e2d1150c00abbc0834f1e5cc47
creal_fpv <cyrilknops@gmail.com>
2020-03-02 10:17:13 +0100

    changed listen-server.c

--- listen-server.c
+++ listen-server.c
@@ -55,7 +55,6 @@ int main(int argc, char const *argv[])
         printf("%s\n",buffer );
         send(new_socket , hello , strlen(hello) , 0 );
         printf("Hello message sent\n");
+        return 0;
     }
-
-    //return 0;
 }
