0ec2a3cd303d7a0470b00786b5b47eca0508035c
creal_fpv <cyrilknops@gmail.com>
2020-03-02 09:59:31 +0100

    changed listen-server.c

--- listen-server.c
+++ listen-server.c
@@ -1,60 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdio.h>
 #include <sys/socket.h>
+#include <stdlib.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <string.h>
+#define PORT 24055
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
 
-#define MYPORT "24055"  // the port users will be connecting to
-#define BACKLOG 10     // how many pending connections queue will hold
-#define MAXDATASIZE 100
-
-int main(void) {
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size;
-    struct addrinfo hints, *res;
-    int sockfd, new_fd,numbytes;
-    char buf[MAXDATASIZE];
-
-    // !! don't forget your error checking for these calls !!
-
-    // first, load up address structs with getaddrinfo():
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-    getaddrinfo(NULL, MYPORT, &hints, &res);
-
-    // make a socket, bind it, and listen on it:
-
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
-    listen(sockfd, BACKLOG);
-
-    // now accept an incoming connection:
-
-    addr_size = sizeof their_addr;
-    while(1) {
-        new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &addr_size);
-        printf("server: got connection");
-        numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0);
-        if (numbytes == -1) {
-            perror("recv");
-            exit(1);
-        } else if (numbytes == 0) {
-            printf("client closed the connection");
-            close(new_fd);
-            exit(0);
-        }
-        buf[numbytes] = '\0';
-        printf("client send: '%s'\n", buf);
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    send(new_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    return 0;
 }
