c79c2d67d9b3d000904a574757773fbdfb7d3fd0
creal_fpv <cyrilknops@gmail.com>
2020-03-02 10:18:36 +0100

    changed listen-server.c

--- listen-server.c
+++ listen-server.c
@@ -50,9 +50,9 @@ int main(int argc, char const *argv[])
         perror("accept");
         exit(EXIT_FAILURE);
     }
-        valread = read( new_socket , buffer, 1024);
-        printf("%s\n",buffer );
-        send(new_socket , hello , strlen(hello) , 0 );
-        printf("Hello message sent\n");
-        return 0;
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    send(new_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    return 0;
 }
