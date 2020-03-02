ff289f51d427531fdc69b57307ff3cc500c8fab9
creal_fpv <cyrilknops@gmail.com>
2020-03-02 09:36:58 +0100

    added listen-server.c

---
+++ listen-server.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define MYPORT "24055"  // the port users will be connecting to
+#define BACKLOG 10     // how many pending connections queue will hold
+#define MAXDATASIZE 100
+
+int main(void) {
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    struct addrinfo hints, *res;
+    int sockfd, new_fd,numbytes;
+    char buf[MAXDATASIZE];
+
+    // !! don't forget your error checking for these calls !!
+
+    // first, load up address structs with getaddrinfo():
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+    getaddrinfo(NULL, MYPORT, &hints, &res);
+
+    // make a socket, bind it, and listen on it:
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    bind(sockfd, res->ai_addr, res->ai_addrlen);
+    listen(sockfd, BACKLOG);
+
+    // now accept an incoming connection:
+
+    addr_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &addr_size);
+
+    numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0);
+    if ( numbytes == -1) {
+        perror("recv");
+        exit(1);
+    }else if (numbytes == 0){
+        printf("client closed the connection");
+        close(new_fd);
+        exit(0);
+    }
+    buf[numbytes] = '\0';
+    printf("client send: '%s'\n",buf);
+}
