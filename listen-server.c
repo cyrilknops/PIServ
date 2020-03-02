02edaa9d47ef041025540f1e818717775e59f744
creal_fpv <cyrilknops@gmail.com>
2020-03-02 10:25:40 +0100

    changed listen-server.c

--- listen-server.c
+++ listen-server.c
@@ -53,8 +53,6 @@ int main(int argc, char const *argv[])
     while(1) {
         valread = read(new_socket, buffer, 1024);
         printf("%s\n", buffer);
-        send(new_socket, hello, strlen(hello), 0);
-        printf("Hello message sent\n");
        //return 0;
     }
 }
