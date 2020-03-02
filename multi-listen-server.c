a4853710dfe99195edab271120d8957b43521ee8
creal_fpv <cyrilknops@gmail.com>
2020-03-02 14:52:40 +0100

    changed listen-server.c

--- multi-listen-server.c
+++ multi-listen-server.c
@@ -127,9 +127,9 @@ int main(int argc , char *argv[])
                 perror("accept");
                 exit(EXIT_FAILURE);
             }
-
+            inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
             //inform user of socket number - used in send and receive commands  
-            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s); , ntohs
+            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntop(their_addr.ss_family, s , ntohs
                     (address.sin_port));
 
             //send new connection greeting message  
