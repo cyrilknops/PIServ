401a13254dd37e374413cf4fb95419ea56736095
creal_fpv <cyrilknops@gmail.com>
2020-03-02 14:32:59 +0100

    changed listen-server.c

--- listen-server.c
+++ listen-server.c
@@ -56,13 +56,13 @@ int main(int argc, char const *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
     while(1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                 (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
         valread = read(new_socket, buffer, 1024);
         printf("%s", buffer);
         addLog(buffer);
