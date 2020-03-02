604750302ba5914fe4bd8cc89eb319ade26ee9cd
creal_fpv <cyrilknops@gmail.com>
2020-03-02 16:03:51 +0100

    changed welcome name

--- multi-listen-server.c
+++ multi-listen-server.c
@@ -25,6 +25,15 @@ void addLog(char message[], char ip[]){
     fprintf(json,"%s @ %s: %s", ip, ts, message);
     fclose(json);
 }
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main(int argc , char *argv[])
 {
