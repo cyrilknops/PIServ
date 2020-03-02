2b2a2bb4179c37c145b3c63f25fcf0703d49b85b
creal_fpv <cyrilknops@gmail.com>
2020-03-02 17:32:05 +0100

    changed welcome name

--- multi-listen-server.c
+++ multi-listen-server.c
@@ -215,9 +215,10 @@ int main(int argc , char *argv[])
                     addLog(buffer, client_name[user]);
                     for (i = 0; i < max_clients; i++) {
                         if(i != user) {
-                            char m[] = client_name[user];
+                            char m[] = "";
+                            m = client_name[user];
                             strcpy(m,": ");
-                            strcpy(m,buffer)
+                            strcpy(m,buffer);
                             send(client_socket[i], m, strlen(m), 0);
                         }
                     }
