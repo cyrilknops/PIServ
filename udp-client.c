53b1945f5ba8d1400fc5cea838abbd17b8ba72ad
creal_fpv <cyrilknops@gmail.com>
2020-03-03 10:58:15 +0100

    changed welcome name

--- udp-client.c
+++ udp-client.c
@@ -6,9 +6,23 @@
 #include<netinet/in.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include <time.h>
 
 #define PORT 24055
 #define MAXLINE 1000
+
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+
+    // Storing start time
+    clock_t start_time = clock();
+
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+}
 
 // Driver code 
 int main()
@@ -33,16 +47,27 @@ int main()
         printf("\n Error : Connect Failed \n");
         exit(0);
     }
+    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("\n Error : Connect Failed \n");
+        exit(0);
+    }
+    while(1){
 
-    // request to send datagram 
-    // no need to specify server address in sendto 
-    // connect stores the peers IP and port 
-    sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)NULL, sizeof(servaddr));
+        // request to send datagram
+        // no need to specify server address in sendto
+        // connect stores the peers IP and port
+        sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)NULL, sizeof(servaddr));
 
-    // waiting for response 
-    recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL);
-    puts(buffer);
+        // waiting for response
+        recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL);
+        puts(buffer);
 
-    // close the descriptor 
+        // close the descriptor
+
+        delay(1);
+    }
     close(sockfd);
+
+
 } 
